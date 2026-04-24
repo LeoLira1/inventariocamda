fa0f4c022a2f8cc4014803e91c5319ec63cbf448
LeoLira1 <128235911+LeoLira1@users.noreply.github.com>
2026-04-24 13:28:27 -0300

    refactor: alinhar prepare script ao fluxo simplificado da revisão

--- scripts/prepare_flutter_project.sh
+++ scripts/prepare_flutter_project.sh
@@ -6,11 +6,7 @@ rm -rf "$PROJECT_DIR"
 
 flutter create --platforms=android "$PROJECT_DIR"
 
-# Remove arquivos de exemplo que podem conflitar com o template real.
-rm -f "$PROJECT_DIR/test/widget_test.dart" "$PROJECT_DIR/lib/main.dart"
-
-# Espelha o template e garante que não fiquem resíduos do projeto gerado.
-rsync -a --delete app_template/ "$PROJECT_DIR"/
+rsync -a app_template/ "$PROJECT_DIR"/
 
 cd "$PROJECT_DIR"
 flutter pub get
